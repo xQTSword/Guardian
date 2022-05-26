6184228eca264efd970139a2a95c48c047c15164
xQTSword <102699949+xQTSword@users.noreply.github.com>
2022-05-26 06:29:30 -0400

    Update Next Update.js

--- Slash Commands/GTA News/Next Update.js
+++ Slash Commands/GTA News/Next Update.js
@@ -1,5 +1,12 @@
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
+const colors = [
+  0xff0000, 0x00ff00, 0x0000ff, 0x00ffff, 0xc72a0c, 0xd9daa5, 0x788770,
+  0x5b7d6c, 0x6e9155, 0x191d1b, 0x6c8c61, 0xb7a589, 0xb0965b, 0x2b1412,
+];
+
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
 if (context.params.event.data.options[0].value === 'nextnewsupdate') {
   await lib.discord.interactions['@1.0.0'].responses.create({
     token: `${context.params.event.token}`,
@@ -14,9 +21,9 @@ if (context.params.event.data.options[0].value === 'nextnewsupdate') {
     embeds: [
       {
         type: 'rich',
-        title: `Next Weekly Update: <t:1653552000:R>`,
+        title: `Next Weekly Update: <t:1654156800:R>`,
         description: '',
-        color: 0x00ffff,
+        color: randomColor,
       },
     ],
   });
